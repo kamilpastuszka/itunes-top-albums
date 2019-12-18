e2e17c1dbe1cf5aa6950421c4878d8af38425265
Kamil <kamil_p7@o2.pl>
2019-12-18 07:45:39 +0100

    added bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React from 'react';
+import Header from './Header';
 
 const App: React.FC = () => {
-  return <div className="App" />;
+    return (
+        <div className="jumbotron">
+            <Header />
+        </div>
+    );
 };
 
 export default App;
---
+++ src/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+    return (
+        <div className="row justify-content-center align-items-center">
+            <h2>Itunes Top Albums</h2>
+        </div>
+    );
+};
+
+export default Header;
